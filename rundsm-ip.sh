64708201d6678da1e29d9638c4245f6df6954f1b
vikramkhatri <vikram@zinox.com>
2018-05-22 20:35:24 -0400

    First Commit

--- rundsm-ip.sh
+++ rundsm-ip.sh
@@ -4,11 +4,13 @@ echo =================================================
 echo Run dsm container
 echo =================================================
 
+IP=${1:-192.168.142.195}
+
 docker run -d -it \
  --name=dsm \
  --env-file=./dsm.env \
  --net mynet \
- --ip 192.168.142.195 \
+ --ip $IP \
  -h dsmhost \
  -p 11080:11080 -p 11081:11081 \
  ibm/dsm:v2.1.5
